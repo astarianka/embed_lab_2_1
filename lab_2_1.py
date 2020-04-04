45192d69fbc2b256de87d5c365c016aad309eee3
Anna Bezverkhnia <annabesverkhnia@gmail.com>
2020-04-05 00:01:06 +0300

    Implement table method
    Signed-off-by: Anna Bezverkhnia <annabesverkhnia@gmail.com>

--- lab_2_1.py
+++ lab_2_1.py
@@ -7,6 +7,11 @@ n = 10
 N = 256
 w = 900
 
+new_coef = 0
+existing_coef = 0
+
+constant_coef = np.zeros((N,N,2))
+
 def signal():
     signal = []
     for i in range(N):
@@ -17,21 +22,30 @@ def signal():
     return signal
 
 def Fp(signal):
+    global new_coef
+    global existing_coef
     fp = []
     for p in range(N):
         fi = 0
         fr = 0
         for k in range(N):
-            fi += signal[k]*math.sin((2*math.pi*p*k)/N)
-            fr += signal[k]*math.cos((2*math.pi*p*k)/N)
+            if (constant_coef[p][k][0] == 0 and constant_coef[p][k][1] == 0):
+                new_coef += 1
+                constant_coef[p][k][0] = math.sin((2*math.pi*p*k)/N)
+                constant_coef[p][k][1] = math.cos((2*math.pi*p*k)/N)
+            else:
+                existing_coef += 1
+            fi += signal[k]*constant_coef[p][k][0]
+            fr += signal[k]*constant_coef[p][k][1]
         fp.append(math.sqrt(math.pow(fr, 2)+math.pow(fi,2)))
     return fp
-
 
 def main():
     sig = signal()
     fp = Fp(sig)
+    f_test = Fp(sig)
     plt.plot(range(N),fp)
+    print(" Added %d coefs, Used %d existed coef" %(new_coef, existing_coef))
     plt.show()
 
 main()
